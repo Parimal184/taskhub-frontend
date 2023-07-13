ae4c16b783449e858ced356ead2d8d73d28ed576
Parimal18 <parimaltandel184@gmail.com>
2023-07-14 00:17:38 +0530

    task class added and removed test files from modal

---
+++ src/app/modal/taskData.ts
@@ -0,0 +1,12 @@
+export class TaskData {
+    title!: string;
+    description!: string;
+    status!: string;
+    priority!: string;
+    creator!: string;
+    assignee!: string;
+    comments!: string
+    created!: string;
+    updated!: string;
+    dueDate!: string;
+}
